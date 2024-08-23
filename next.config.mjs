75298527d8cdc6f6c496775442a972eb8ed1ce8d
Leigh Dahlin <leigh.dahlin@gmail.com>
2024-08-23 17:52:39 +0200

    setup decap cms

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
     output: 'export',
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',  
+    webpack: (cfg) => {
+        cfg.module.rules.push({
+          test: /\.md$/,
+          loader: 'frontmatter-markdown-loader',
+          options: { mode: ['react-component'] },
+        });
+        return cfg;
+      },
 };
 
 export default nextConfig;
